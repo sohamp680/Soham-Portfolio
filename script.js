0a23d9985cacc93fc456ac9611c4bd4cb29f3e5d
soham <sohamp680@gmail.com>
2023-02-16 11:28:54 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+// Set the timer
+var theDelay = 1;
+function timer() {
+    setTimeout( function(){
+        toggleText();    
+    }, theDelay * 1000 );
+}
+// Call the timer
+timer();
+
+// Toggle visibility
+function toggleText() {
+    document.getElementById("toShow").style.visibility = "visible";
+    document.getElementById("toHide").style.visibility = "hidden";
+}
+*/
